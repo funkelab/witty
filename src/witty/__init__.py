660093f3ef0fe94bbc02b712e8fb73429bd070da
Talley Lambert <talley.lambert@gmail.com>
2025-07-21 18:37:46 -0400

    update for uv

--- src/witty/__init__.py
+++ src/witty/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("witty")
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
     # package is not installed
     __version__ = "unknown"
 
